8b9afc6aae78ffcb1ec3691bcb32d669b0b930ea
Gaurav Sharma <79034287+iamgauravsharma2084@users.noreply.github.com>
2022-04-08 18:20:15 +0530

    add two string using STL.cpp

---
+++ add two string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   string s1,s2,s3;
+   cout<<"Enter two string:-";
+   getline(cin,s1);
+   getline(cin,s2);
+   s3=s1+s2;
+   cout<<s3<<" ";
+}
