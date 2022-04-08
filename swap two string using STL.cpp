5d954f36f3225ba00c885150f38ae0f728134a2f
Gaurav Sharma <79034287+iamgauravsharma2084@users.noreply.github.com>
2022-04-08 18:22:42 +0530

    Swap two string using STL.cpp

---
+++ swap two string using STL.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   string s1,s2,s3;
+   cout<<"Enter two string:-";
+   getline(cin,s1);
+   getline(cin,s2);
+   s3=s1;
+   s1=s2;
+   s2=s3;
+   cout<<s1<<" "<<s2;
+}
