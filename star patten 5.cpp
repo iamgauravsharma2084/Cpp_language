855d0207653c1d8880e5e1e831f9c599efff1679
Gaurav Sharma <79034287+iamgauravsharma2084@users.noreply.github.com>
2022-04-15 14:23:21 +0530

    Add files via upload

--- star patten 5.cpp
+++ star patten 5.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
 using namespace std;
-main()
+int main()
 {
-  int i;
- int j;;
-  for(i=1;i<=5;i++)
-  {
-  	for(j=1;j<=9;j++)
-  	{
-  	  if(j>=6-i&&j<=4+i)
-		 cout<<"*";
-	   else
-	     cout<<" ";	
-	  }
-	   cout<<"\n";
-  }
- 
-
+    int i,j,l;
+    for(i=1;i<=5;i++)
+    {
+        l=1;
+        for(j=0;j<=9;j++)
+        {
+            if(j>=6-i&&j<=4+i&&l==1)
+            {
+              cout<<"*";
+              l=0;
+            }
+            else
+            {
+              cout<<" ";
+              l=1;
+            }
+        }
+        cout<<endl;
+    }
 }
