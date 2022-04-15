cb3a3d93d208cf2519fa1924d025ebc17305b37c
Gaurav Sharma <79034287+iamgauravsharma2084@users.noreply.github.com>
2022-04-15 14:03:29 +0530

    Add files via upload

---
+++ calloc function.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *p,i,n;
+     int sum=0;
+    cout<<"Enter your number:-";
+    cin>>n;
+    p=(int*)calloc(n,sizeof(int));
+    for(i=0;i<n;i++)
+    {
+       cin>>*(p+i);
+    }
+    for(i=0;i<n;i++)
+    {
+         sum=sum+*(p+i);
+    }
+    cout<<sum;
+    free (p);
+}
