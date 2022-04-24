95d4fc344d26d24de30710fa94349b440e2fd7ce
Gaurav Sharma <79034287+iamgauravsharma2084@users.noreply.github.com>
2022-04-24 10:30:34 -0700

    Add files via upload

--- without main name print.cpp
+++ without main name print.cpp
@@ -1,7 +1,41 @@
 #include<iostream>
-#define start main
 using namespace std;
-start()
+class B;
+class A
 {
-  cout<<"Hello Chetu";	
+   private:
+   	  int a;
+   	public:
+   		void add()
+   		{
+   			cout<<"Enter your number:-";
+   			cin>>a;
+		}  
+   	friend void Fun(A,B);
+};
+class B:A
+{
+    private:
+   	  int b;
+   	public:
+   		void ad()
+   		{
+   			cout<<"Enter your number:-";
+   			cin>>b;
+		}  
+   	friend void Fun(A,B);
+};
+void Fun(A x,B y)
+{
+	int c;
+	c=x.a+y.b;
+	cout<<c;
+}
+main()
+{
+	 A a;
+	 B b;
+	 a.add();
+	 b.ad();
+	Fun(a,b);
 }
